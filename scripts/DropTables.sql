72f802eee0677c2cf4e1980d057fcc93645d5f34
Inbenglable <1321156731@qq.com>
2022-12-05 11:07:37 +0800

    drop user

--- scripts/DropTables.sql
+++ scripts/DropTables.sql
@@ -1,6 +1,14 @@
+
+drop owned by sustc_manager;
 drop user sustc_manager;
+
+drop owned by courier;
 drop user courier;
+
+drop owned by company_manager;
 drop user company_manager;
+
+drop owned by seaport_officer;
 drop user seaport_officer;
 
 drop table container_item;
@@ -20,3 +28,7 @@ drop table company;
 drop table item;
 drop table container;
 drop table item_type;
+
+
+--select rolname from pg_roles;  查user列表
+--select * from information_schema.table_privileges where grantee='seaport_officer';  查某个user的权限
