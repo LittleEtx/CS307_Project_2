fa2fd916b3124e8b18c8615f8a5f4a344351e1e8
Inbenglable <1321156731@qq.com>
2022-12-17 22:06:43 +0800

    Finish methods in courier and SeaportOfficer

--- src/main/java/com/littleetx/cs307_project_2/database/user/Courier.java
+++ src/main/java/com/littleetx/cs307_project_2/database/user/Courier.java
@@ -45,7 +45,9 @@ public class Courier extends User {
      * already exist or contains illegal information
      */
     public boolean newItem(ItemInfo item) {
-        //TODO
+        if (GlobalQuery.getStaffId(item.retrieval().courier())!=this.id){
+            return false;
+        }
         try {
             PreparedStatement stmt=conn.prepareStatement("select * from item where name= ? ");
             stmt.setString(1,item.name());
@@ -79,9 +81,10 @@ public class Courier extends User {
 
                 stmt= conn.prepareStatement("insert into staff_handle_item values(?,?,?)");
                 stmt.setString(1,item.name());
-                stmt.setInt(2,GlobalQuery.getStaffId(item.retrieval().courier()));
+                stmt.setInt(2,this.id);
                 stmt.setString(3,"RETRIEVAL");
                 stmt.execute();
+                return true;
             }
         }catch (SQLException e){
             throw new RuntimeException(e);
@@ -95,7 +98,81 @@ public class Courier extends User {
      * @return false if item already exist or s is illegal
      */
     public boolean setItemState(String itemName, ItemState itemState) {
-        //TODO
+        try {
+            PreparedStatement stmt=conn.prepareStatement("select state from item_state where item_name= ? ");
+            stmt.setString(1,itemName);
+            ResultSet rs=stmt.executeQuery();
+            if (rs.next()){
+                String item_Fstate=rs.getString(1);
+
+                stmt=conn.prepareStatement("select stage from staff_handle_item where item_name= ? and  staff_id= ? and stage= ? ");
+                stmt.setString(1,itemName);
+                stmt.setInt(2,this.id);
+                if (itemState.equals(ItemState.ToExportTransporting)&&
+                        item_Fstate.equals(DatabaseMapping.getStateDatabaseString(ItemState.PickingUp))){
+                    stmt.setString(3,"RETRIEVAL");
+                }
+                else if (itemState.equals(ItemState.ExportChecking)&&
+                        item_Fstate.equals(DatabaseMapping.getStateDatabaseString(ItemState.ToExportTransporting))){
+                    stmt.setString(3,"RETRIEVAL");
+                }
+                else if (itemState.equals(ItemState.FromImportTransporting)&&
+                        item_Fstate.equals(DatabaseMapping.getStateDatabaseString(ItemState.FromImportTransporting))){
+                    stmt=conn.prepareStatement("select stage from staff_handle_item where item_name= ? and stage= ? ");
+                    stmt.setString(1,itemName);
+                    stmt.setString(2,"DELIVERY");
+                }
+                else if (itemState.equals(ItemState.Delivering)&&
+                        item_Fstate.equals(DatabaseMapping.getStateDatabaseString(ItemState.FromImportTransporting))){
+                    stmt.setString(3,"DELIVERY");
+                }
+                else if (itemState.equals(ItemState.Finish)&&
+                        item_Fstate.equals(DatabaseMapping.getStateDatabaseString(ItemState.Delivering))){
+                    stmt.setString(3,"DELIVERY");
+                }
+                else{
+                    return false;
+                }
+                rs=stmt.executeQuery();
+                if (itemState.equals(ItemState.FromImportTransporting)){
+                    if (rs.next()){
+                        return false;
+                    }
+                    else{
+                        stmt=conn.prepareStatement("select city_id from item_route where item_name= ? and stage= 'DELIVERY' ");
+                        stmt.setString(1,itemName);
+                        rs=stmt.executeQuery();
+                        rs.next();
+                        int itemCity=rs.getInt(1);
+                        stmt=conn.prepareStatement("select city_id from staff_city where staff_id = ? ");
+                        stmt.setInt(1,this.id);
+                        rs=stmt.executeQuery();
+                        rs.next();
+                        int staffCity=rs.getInt(1);
+                        if (itemCity==staffCity){
+                    stmt=conn.prepareStatement("insert into staff_handle_item values(?,?,?)");
+                    stmt.setString(1,itemName);
+                    stmt.setInt(2,this.id);
+                    stmt.setString(3,"DELIVERY");
+                    stmt.execute();
+                    return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                }
+              else if (rs.next()){
+                    stmt=conn.prepareStatement("update item_state set state= ? where item_name= ? ");
+                    stmt.setString(1,DatabaseMapping.getStateDatabaseString(itemState));
+                    stmt.setString(2,itemName);
+                    stmt.execute();
+                    return true;
+                }
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
         return false;
     }
 
